6632d63117d347cbd989f4e7921024a4eadb6ab7
thespecialone1 <s3neo36@gmail.com>
2024-12-28 15:32:08 +0400

    added srvr fnctionality

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module githhub.com/thespecialone1/rssfeed-scraper
 
 go 1.23.3
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi/v5 v5.2.0
--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
-func main(){
-	fmt.Println("Hey yo")
-	portString:= os.Getenv("PORT")
-	if portString == "" {
-		log.Fatal("PORT is not found in the enviorment")
-	} else{
-		fmt.Println("PORT IS:", portString)
+func main() {
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == ""{
+		log.Fatal("Port is not found in the envPath: ")
+	}
+
+	router := chi.NewRouter()
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":"+portString,
+	}
+	log.Printf("Server is listening on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil{
+		log.Fatal(err)
 	}
 }
